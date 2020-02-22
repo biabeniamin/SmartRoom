f2ffdf2540c238afc96afc36cdbd328700382868
Beniamin Bia <biabeniamin@outlook.com>
2020-02-22 15:01:10 +0000

    install apache and copy server

--- PythonServer/install.sh
+++ PythonServer/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install python3-pip
-sudo pip3 install serial
+sudo apt-get install python-pip -y
+sudo pip install pyserial
+sudo pip install RPi.GPIO
+sudo apt-get install apache2 -y
+sudo rm /var/www/html/* -r
+sudo cp ../Raspberry-WebServer/* /var/www/html/. -r
