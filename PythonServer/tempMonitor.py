54281f4c6b9030a6f5565ea725bb6a33de1f71b8
Beniamin Bia <biabeniamin@outlook.com>
2020-02-25 18:24:31 +0000

    add decimal point in temperature conversion

--- PythonServer/tempMonitor.py
+++ PythonServer/tempMonitor.py
@@ -4,9 +4,10 @@ def readTemperature():
 	while 1:
 		f = open('/sys/bus/w1/devices/28-000008a82661/hwmon/hwmon1/temp1_input','r')	
 		temperature = f.read()
+		temperature=float(temperature) / 1000
 		f.close()
 		f = open('/var/www/html/roomStatus/temperature','w')	
-		f.write(temperature)
+		f.write(str(temperature))
 		f.close()
 		time.sleep(10)
 
