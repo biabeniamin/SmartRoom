1103ebf9ac18204cb0e825ec42a2a03c60fa0040
Beniamin Bia <biabeniamin@outlook.com>
2020-03-04 15:38:14 +0200

    install php automatically

--- PythonServer/install.sh
+++ PythonServer/install.sh
@@ -4,5 +4,6 @@ sudo apt-get install python-pip -y
 sudo pip install pyserial
 sudo pip install RPi.GPIO
 sudo apt-get install apache2 -y
+sudo apt install php libapache2-mod-php -y
 sudo rm /var/www/html/* -r
 sudo cp ../Raspberry-WebServer/* /var/www/html/. -r
